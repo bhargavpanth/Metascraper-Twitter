4c7ae3893c8fa3c735d8d01ec66e3b6434344caf
Bhargav Panth <bhargavrpanth@gmail.com>
2020-11-02 20:12:51 +0000

    added tsc transpilation

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ssr = void 0;
+const puppeteer_1 = __importDefault(require("puppeteer"));
+async function ssr(url) {
+    const browser = await puppeteer_1.default.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const html = await page.content(); // serialized HTML of page DOM.
+    await browser.close();
+    return html;
+}
+exports.ssr = ssr;
