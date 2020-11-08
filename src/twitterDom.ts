8805cd5c1eae9a2370ffa85099c1bbaea67ba97a
Bhargav Panth <bhargavrpanth@gmail.com>
2020-11-08 00:14:18 +0000

    minor change in TwitterDOM class

--- src/twitterDom.ts
+++ src/twitterDom.ts
@@ -1,12 +1,20 @@
-/* Make Twitter DOM elements for all the previewable content */
 import { DOMParser } from './domParser'
 
+/**
+ * @params stringified html document
+ * **/
 export class TwitterDOM extends DOMParser {
     constructor(html: string) {
         super(html)
     }
-
+    /* Author name */
     get author() {
+        return super.DOM.window.document.getElementsByClassName(
+            'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'
+        )
+    }
+    /* Twitter handle */
+    get handle() {
         return super.DOM.window.document.getElementsByClassName(
             'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'
         )
