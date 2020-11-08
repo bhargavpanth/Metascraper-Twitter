27491e4e07d25d3d1183ce4b5b826c684eaf4e5c
Bhargav Panth <bhargavrpanth@gmail.com>
2020-11-08 00:07:31 +0000

    moved getter to TwitterDOM class

--- src/domParser.ts
+++ src/domParser.ts
@@ -15,8 +15,4 @@ export class DOMParser {
     get DOM() {
         return this.dom
     }
-
-    get author() {
-        return this.dom.window.document.getElementsByClassName('css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0')
-    }
 }
--- src/twitterDom.ts
+++ src/twitterDom.ts
@@ -1,6 +1,14 @@
 /* Make Twitter DOM elements for all the previewable content */
 import { DOMParser } from './domParser'
 
-class TwitterDOM extends DOMParser {
-    //
+export class TwitterDOM extends DOMParser {
+    constructor(html: string) {
+        super(html)
+    }
+
+    get author() {
+        return super.DOM.window.document.getElementsByClassName(
+            'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'
+        )
+    }
 }
