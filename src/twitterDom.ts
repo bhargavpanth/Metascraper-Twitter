08389229201065e8eb49ca04964ca6db1bac9543
Bhargav Panth <bhargavrpanth@gmail.com>
2020-11-09 00:10:53 +0000

    minor change

--- src/twitterDom.ts
+++ src/twitterDom.ts
@@ -10,15 +10,23 @@ export class TwitterDOM extends TwitterDOMParser {
 
     /* Author name */
     get author() {
-        return super.DOM.window.document.getElementsByClassName(
+        return super.DOM.window.document.querySelector('span').getElementsByClassName(
             'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'
         ).textContent
     }
 
     /* Twitter handle */
     get handle() {
-        return super.DOM.window.document.getElementsByClassName(
+        return super.DOM.window.document.querySelector('span').getElementsByClassName(
             'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0'
         ).textContent
+    }
+
+    get timeAndDate() {
+        return
+    }
+
+    profilePicture() {
+        return
     }
 }
